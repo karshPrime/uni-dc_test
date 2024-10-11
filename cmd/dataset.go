035d25d302e119c2fedf2531a4f89982864724bc
karsh <utkarsh@cyberfear.com>
2024-10-12 05:14:24 +1100

    dataset
    - removed indie definition. all sets are defined within the array itself
    ~ reformatted datasets to have one single valid true case, and three
      iterations for different purpose
    - removed TestResult struct - redundant

--- cmd/dataset.go
+++ cmd/dataset.go
@@ -1,59 +1,53 @@
 
 package cmd
 
-type ProgramIO struct {
-    IStart  string
-    IEnd    string
-
-    MStart  string
-    MEnd    string
-
+type ProgramInputs struct {
+    Start   string
+    End     string
     Difference int
 }
 
-type TestResult struct {
-    IResult     int
-    MResult     int
-    TestCase    bool
+var TestValues = [5]ProgramInputs {
+    { "18/02/2015", "27/03/2016", 0 }, // covers 1 leap day
+    { "15/01/2012", "05/02/2013", 0 },
+    { "11/04/2000", "20/05/2003", 0 },
+    { "08/09/2023", "17/10/2024", 0 }, // covers 1 leap day
+    { "08/08/2024", "27/09/2026", 0 },
+    // difference field doesn't make logical sense for the test struct
+    // but is here to comply with the struct definition
 }
 
+//- Metamorphic Tests ----------------------------------------------------------
 
-//- Datasets -------------------------------------------------------------------
+var MetamorphicExpected = [3][5]ProgramInputs {
+    {// Start becomes End. End becomes Start
+        { "27/03/2016", "18/02/2015", 0 },
+        { "05/02/2013", "15/01/2012", 0 },
+        { "20/05/2003", "11/04/2000", 0 },
+        { "17/10/2024", "08/09/2023", 0 },
+        { "27/09/2026", "08/08/2024", 0 },
+        // expected difference is 0, since dates are just swapped
+    },
 
-// start becomes end, end becomes start
-var MetamorphicValuesSet1 = [5]ProgramIO {
-    // InitialStart, InitialEnd , ModifiedStart, ModifiedEnd, Difference
-    { "05/01/2020", "15/05/2022", "15/05/2022", "05/01/2020", 0 },
-    { "12/08/2018", "22/09/2019", "22/09/2019", "12/08/2018", 0 },
-    { "03/11/2021", "14/12/2020", "14/12/2020", "03/11/2021", 0 },
-    { "25/04/2015", "30/06/2016", "30/06/2016", "25/04/2015", 0 },
-    { "09/02/2022", "18/03/2023", "18/03/2023", "09/02/2022", 0 },
+    {// Changed Years for Start and End with same constant
+        { "18/02/2007", "27/03/2008", 0 }, // -8
+        { "15/01/1912", "05/02/1913", 0 }, // -100
+        { "11/04/2024", "20/05/2027", 0 }, // +24
+        { "08/09/2055", "17/10/2056", 0 }, // +32
+        { "08/08/2124", "27/09/2126", 0 }, // +100
+        // expected difference is 0, since years difference is kept consistent
+    },
+
+    {// Different dates for Same Month and Same Year
+        { "08/02/2015", "27/03/2016", +10 },
+        { "25/01/2012", "05/02/2013", -10 },
+        { "01/04/2000", "10/05/2003",   0 }, // rise in one balances other's fall
+        { "01/09/2023", "30/10/2024", +20 },
+        { "28/08/2024", "07/09/2026", -40 },
+        // increasing start = decreases days count
+        // increasing end   = increases days count
+    },
 }
 
-// changed years with same difference
-var MetamorphicValuesSet2 = [5]ProgramIO {
-    // InitialStart, InitialEnd , ModifiedStart, ModifiedEnd, Difference
-    { "10/10/2018", "20/11/2019", "10/10/2009", "20/11/2010", 0 },
-    { "01/01/2021", "12/12/2022", "01/01/1990", "12/12/1991", 0 },
-    { "15/07/2017", "25/08/2018", "15/07/2041", "25/08/2042", 0 },
-    { "22/03/2020", "30/04/2021", "22/03/2037", "30/04/2038", 0 },
-    { "05/06/2016", "14/07/2017", "05/06/1902", "14/07/1903", 0 },
-}
-
-// Different dates for Same Month and Same Year
-var MetamorphicValuesSet3 = [5]ProgramIO {
-    // InitialStart, InitialEnd , ModifiedStart, ModifiedEnd, Difference
-    { "18/02/2015", "27/03/2016", "28/02/2015", "27/03/2016", -10 },
-    { "08/09/2023", "17/10/2024", "08/09/2023", "27/10/2024", +10 },
-    { "15/01/2012", "05/02/2013", "25/01/2012", "15/02/2013",   0 },
-    { "11/04/2019", "20/05/2020", "01/04/2019", "30/05/2020", +20 },
-    { "08/08/2025", "27/09/2026", "28/08/2025", "07/09/2026", -40 },
-}
-
-// Array of [5]ProgramIO arrays
-var MetamorphicValues = [3][5]ProgramIO {
-    MetamorphicValuesSet1,
-    MetamorphicValuesSet2,
-    MetamorphicValuesSet3,
-}
+//------------------------------------------------------------------------------
 
