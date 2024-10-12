e58bd37689412689edb5bba185f572abd8e55d52
karsh <utkarsh@cyberfear.com>
2024-10-12 11:20:50 +1100

    update main
    + added mutated package
    + added way to differentiate mutation vs original code function call
    + added logic to run metamorphic tests for number of times

--- main.go
+++ main.go
@@ -6,42 +6,78 @@ import (
     "github.com/karshPrime/uni-dc_test/cmd"
 
     "github.com/wy3148/dc/date" // Program Under Test
+    "github.com/karshPrime/uni-dc_test/mutated" // Mutated Program Source
 )
 
-func metamorphicTests( aCase int ) {
-    cmd.TableHead()
+const DEFAULT = 0;
+var TEST_PURPOSE = [3]string {
+    "Inverted Start & End",
+    "Same Date, Different Years",
+    "Different Date, Same Month & Year",
+}
 
-    for i := 0; i < 5; i++ {
-        lIResult, _ := date.Elapsed(
-            cmd.MetamorphicValues[aCase-1][i].IStart,
-            cmd.MetamorphicValues[aCase-1][i].IEnd )
+func metamorphicTests( aCase, aCondition int ) {
+    fmt.Printf( "Metamorphic Test %v: %v\n", aCase, TEST_PURPOSE[aCase-1] )
+    cmd.MetamorphicTableHead()
 
-        lMResult, _ := date.Elapsed(
-            cmd.MetamorphicValues[aCase-1][i].MStart,
-            cmd.MetamorphicValues[aCase-1][i].MEnd )
+    /* Got identical loop bellow to not have if-else check on every loop
+     * Sure, this reduces code reusability, but in this case i reckon its
+     * justified.
+     *
+     * Could have used a functional variable for Data.Elapsed(...) and set that
+     * with 'if' before loop, though that'd require making changes to wy3148/dc
+     * since my mutated dc takes in an extra parameter.
+     *
+     * 'aCondition' is used to select specific change in dc, instead of editing
+     * source code again and again for every test.
+     */
 
+    if aCondition == DEFAULT {
+        // original function at wy3148/dc
+        for i := 0; i < 5; i++ {
+            lIResult, _ := date.Elapsed(
+                cmd.TestValues[i].Start,
+                cmd.TestValues[i].End )
 
-        lTestResult := cmd.TestResult {
-            IResult: lIResult,
-            MResult: lMResult,
+            lMResult, _ := date.Elapsed(
+                cmd.MetamorphicExpected[aCase-1][i].Start,
+                cmd.MetamorphicExpected[aCase-1][i].End )
 
-            TestCase: lMResult ==
-                ( lIResult + cmd.MetamorphicValues[aCase-1][i].Difference ),
+            cmd.MetamorphicTableData( (aCase-1), i, lIResult, lMResult )
         }
+    } else {
+        // modified
+        for i := 0; i < 5; i++ {
+            lIResult, _ := mutated.Elapsed( aCondition,
+                cmd.TestValues[i].Start,
+                cmd.TestValues[i].End )
 
-        cmd.TableData( cmd.MetamorphicValues[aCase-1][i], lTestResult )
+            lMResult, _ := mutated.Elapsed( aCondition,
+                cmd.MetamorphicExpected[aCase-1][i].Start,
+                cmd.MetamorphicExpected[aCase-1][i].End )
+
+            cmd.MetamorphicTableData( (aCase-1), i, lIResult, lMResult )
+        }
     }
-    cmd.TableEnd()
+
+    cmd.MetamorphicTableEnd()
+    fmt.Println("")
 }
 
 func main() {
-    // fmt.Println(" Metamorphic Test 1: Inverted Start & End")
-    // metamorphicTests(1)
+    // Original Code Runs
+    metamorphicTests(1, DEFAULT)
+    metamorphicTests(2, DEFAULT)
+    metamorphicTests(3, DEFAULT)
 
-    // fmt.Println(" Metamorphic Test 2: Same Date, Different Years")
-    // metamorphicTests(2)
-    //
-    fmt.Println("Metamorphic Test 3: Different Date, Same Month & Year")
-    metamorphicTests(3)
+    // Mutation Test
+    for i := 0; i < 30; i++ {
+        fmt.Printf("\n|==[ Checking Mutant %02d ", i+1 )
+        cmd.PrintRepeat( "]", "=", "|", 66, true )
+
+        metamorphicTests(1, i)
+        metamorphicTests(2, i)
+        metamorphicTests(3, i)
+    }
 }
 
