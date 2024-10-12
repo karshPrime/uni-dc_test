5d8fb259104c5656d557fed81bf0a06973f6554a
karsh <utkarsh@cyberfear.com>
2024-10-13 05:35:28 +1100

    mutations
    + added mutations to the source code corresponding to report table

--- mutated/cal.go
+++ mutated/cal.go
@@ -6,22 +6,57 @@ import (
 
 func caculate(aConditions int, start *date, end *date) int {
 
-	//UTC timestamp in seconds
-	t1 := time.Date(start.year, time.Month(start.month), start.day, 0, 0, 0, 0, time.UTC).Unix()
-	t2 := time.Date(end.year, time.Month(end.month), end.day, 0, 0, 0, 0, time.UTC).Unix()
+	// UTC timestamp in seconds
+	t2 := time.Date(end.year, time.Month(end.month),
+        end.day, 0, 0, 0, 0, time.UTC).Unix()
 
-	//end date is ealier, swap them
-	if t1 > t2 {
-		t1, t2 = t2, t1
-	}
+    var t1 int64
+    if aConditions == 1 { // mutation 1
+	    t1 = time.Date(start.year, time.Month(start.month),
+            start.day, 1, 0, 0, 0, time.UTC).Unix()
+    } else {
+	    t1 = time.Date(start.year, time.Month(start.month),
+            start.day, 0, 0, 0, 0, time.UTC).Unix()
+    }
 
-	elapsed := int((t2-t1)/(3600*24) - 1)
+	// end date is ealier, swap them
+    switch aConditions {
+        // mutants
+        case 12: if t1 < t2 { t1, t2 = t2, t1 }
+        case 13: if !(t1 > t2) { t1, t2 = t2, t1 }
+        case 14: if t1 > t2 { t1, t2 = t2, t2 }
+        case 15: if t1 > t2 { t1, t2 = t1, t2 }
 
-	if elapsed > 0 {
-		return elapsed
-	}
+        // original code
+	    default: if t1 > t2 { t1, t2 = t2, t1 }
+    }
 
-	return 0
+    var elapsed int
+    switch aConditions {
+        // mutants
+        case 2 : elapsed = int( ( t2 - t1 ) / ( 3600 * 23 ) - 1 )
+        case 3 : elapsed = int( ( t2 - t1 ) / ( 3600 * 24 ) - 2 )
+        case 4 : elapsed = int( ( t2 - t1 ) / ( 3600 * 24 ) + 1 )
+        case 5 : elapsed = int( ( t2 + t1 ) / ( 3600 * 24 ) - 1 )
+        case 6 : elapsed = int( ( t2 * t1 ) / ( 3600 * 24 ) - 1 )
+        case 7 : elapsed = int( ( t2 / t1 ) / ( 3600 * 24 ) - 1 )
+
+        // original code
+        default: elapsed = int( ( t2 - t1 ) / ( 3600 * 24 ) - 1 )
+    }
+
+    switch aConditions {
+        // mutants
+        case 8 : return 1 // return 1 before the function's final return 0
+        case 9 : return 0 // return nothing; skip to final return 0
+        case 16: if elapsed != 0 { return elapsed }
+        case 17: if elapsed < 0 { return elapsed }
+
+        // original behaviour
+        default: if elapsed > 0 { return elapsed }
+    }
+
+    return 0
 }
 
 // Elapsed caculate the eclapsed days
@@ -40,6 +75,13 @@ func Elapsed(aConditions int, start, end string) (int, error) {
 		return -1, err
 	}
 
-	return caculate(aConditions, startD, endD), nil
+    switch aConditions {
+        // mutants
+        case 10: return caculate(aConditions, endD, startD), nil
+        case 11: return caculate(aConditions, startD, startD), nil
+
+        // original behaviour
+        default: return caculate(aConditions, startD, endD), nil
+    }
 }
 
--- mutated/parse.go
+++ mutated/parse.go
@@ -61,16 +61,77 @@ func isValidDate(aCondition int, d *date) (bool, error) {
 
 	//The solution needs to cater for all valid dates between
 	//01/01/1901 and 31/12/2999.
-	if d.year < 1901 || d.year > 2999 ||
-		d.month < 1 || d.month > 12 ||
-		d.day < 1 || d.day > 31 {
-		return false, fmt.Errorf("the date time must be within the correct time range:01/01/1901 - 31/12/2999")
-	}
+    switch aCondition {
+        // mutatants
+        case 18:
+            if d.year < 1901 || d.year > 3000 ||
+            d.month < 1 || d.month > 12 ||
+            d.day < 1 || d.day > 31 {
+                return false, fmt.Errorf("the date time must be within the correct time range:01/01/1901 - 31/12/2999")
+            }
+
+        case 19:
+            if d.year < 1901 && d.year > 2999 ||
+            d.month < 1 || d.month > 12 ||
+            d.day < 1 || d.day > 31 {
+                return false, fmt.Errorf("the date time must be within the correct time range:01/01/1901 - 31/12/2999")
+            }
+
+        case 20:
+            if d.year < 1901 || false ||
+            d.month < 1 || d.month > 12 ||
+            d.day < 1 || d.day > 31 {
+                return false, fmt.Errorf("the date time must be within the correct time range:01/01/1901 - 31/12/2999")
+            }
+
+        // original code
+        default:
+            if d.year < 1901 || d.year > 2999 ||
+            d.month < 1 || d.month > 12 ||
+            d.day < 1 || d.day > 31 {
+                return false, fmt.Errorf("the date time must be within the correct time range:01/01/1901 - 31/12/2999")
+            }
+    }
 
 	if isLeapYear(aCondition, d.year) {
-		if d.day > leapDays[d.month] {
-			return false, fmt.Errorf("the day of the month is out of range,it should be between 1 - %d", leapDays[d.month])
-		}
+        switch aCondition {
+            // mutations
+            case 21:
+                if d.day < leapDays[d.month] {
+                    return false, 
+                    fmt.Errorf("the day of the month is out of range,it should be between 1 - %d",
+                    leapDays[d.month])
+                }
+
+            case 22:
+                if d.day >= leapDays[d.month] {
+                    return false, 
+                    fmt.Errorf("the day of the month is out of range,it should be between 1 - %d",
+                    leapDays[d.month])
+                }
+
+            case 23:
+                if d.day > leapDays[d.month+1] {
+                    return false, 
+                    fmt.Errorf("the day of the month is out of range,it should be between 1 - %d",
+                    leapDays[d.month])
+                }
+
+            case 24:
+                if d.day > leapDays[1] {
+                    return false, 
+                    fmt.Errorf("the day of the month is out of range,it should be between 1 - %d",
+                    leapDays[d.month])
+                }
+
+            // original code
+            default:
+                if d.day > leapDays[d.month] {
+                    return false, 
+                    fmt.Errorf("the day of the month is out of range,it should be between 1 - %d",
+                    leapDays[d.month])
+                }
+        }
 	} else {
 		if d.day > nonLeapDays[d.month] {
 			return false, fmt.Errorf("the day of the month is out of range,it should be betwen 1- %d", nonLeapDays[d.month])
@@ -90,10 +151,24 @@ func parse(aCondition int, str string) (*date, error) {
 		return nil, fmt.Errorf("failed to parse the input time:%s, %s", str, " It has to be DD/MM/YYYY format exactly")
 	}
 
-	//matchDate make sure we get the digits string, we don't need to check err here
-	d.day, _ = strconv.Atoi(res[0][1])
-	d.month, _ = strconv.Atoi(res[0][2])
-	d.year, _ = strconv.Atoi(res[0][3])
+	// matchDate make sure we get the digits string, we don't need to check err here
+    switch aCondition {
+        case 25: d.day, _ = strconv.Atoi(res[0][0])
+        case 26: d.day, _ = strconv.Atoi(res[1][1])
+        default: d.day, _ = strconv.Atoi(res[0][1])
+    }
+
+    switch aCondition {
+        case 27: d.month, _ = strconv.Atoi(res[0][0])
+        case 28: d.month, _ = strconv.Atoi(res[2][2])
+        default: d.month, _ = strconv.Atoi(res[0][2])
+    }
+
+    switch aCondition {
+        case 29: d.year, _ = strconv.Atoi(res[0][0])
+        case 30: d.year, _ = strconv.Atoi(res[3][3])
+        default: d.year, _ = strconv.Atoi(res[0][3])
+    }
 
 	_, err := isValidDate(aCondition, &d)
 
